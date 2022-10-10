99df721b39602a569b19a9115c33533722eeaea7
july <jaruiz0683@misena.edu.co>
2022-10-10 12:07:52 -0500

    avancesxd

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { dirname } = require('path')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('view engine' , 'ejs')
+app.set('views', __dirname+'/views')
+
+
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+  res.render('index')
+})
+
+app.get('/Educacion', (req, res) => {
+    res.render('educacion',{
+        titulo: "Seccion educativa",
+        descripcion: "aquí verás mi info Académica"
+    })
+  })
+
+app.get('/Laboral', (req, res) => {
+    res.send('nivel Laboral')
+  })  
+
+  app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname+'/public/404.html')
+})
+
+app.listen(port, () => {
+  console.log(`puedes acceder al servidor haciendo click aquí http://localhost:${port}`)
+})
+
